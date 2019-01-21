f6714bbfada2145d94179b74b5c5aca46982d9ff
Esa <helius_aim00@yahoo.com>
2019-01-21 23:09:57 +0700

    first commit

---
+++ app/src/main/java/com/example/esa/ecalori/catatanHarian2.java
@@ -0,0 +1,13 @@
+package com.example.esa.ecalori;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class catatanHarian2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catatan_harian2);
+    }
+}
