54acbda30b5c18440e13abafab110134ffb65e0e
esa <helius_aim00@yahoo.com>
2019-01-22 14:44:06 +0700

    perbaikan eror slide menu

--- app/src/main/java/com/example/esa/ecalori/catatanHarian2.java
+++ app/src/main/java/com/example/esa/ecalori/catatanHarian2.java
@@ -1,7 +1,10 @@
 package com.example.esa.ecalori;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class catatanHarian2 extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class catatanHarian2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catatan_harian2);
-    }
+
+        Button aktiv0 = (Button) findViewById(R.id.aktiv0);
+        aktiv0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =  new Intent(catatanHarian2.this, catatanharian3.class);
+                startActivity(i);
+            }
+    });
+}
 }
