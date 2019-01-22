d7a278936e14fb62c0aac87f2f959e059b4a8456
esa <helius_aim00@yahoo.com>
2019-01-22 11:43:34 +0700

    perbaikan eror slide menu

--- app/src/main/java/com/example/esa/ecalori/catatanharian3.java
+++ app/src/main/java/com/example/esa/ecalori/catatanharian3.java
@@ -8,6 +8,6 @@ public class catatanharian3 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_catatanharian3);
+        setContentView(R.layout.activity_catatan_harian3);
     }
 }
